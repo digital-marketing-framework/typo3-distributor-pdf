56df6f023b07fb60417a961eeb5eb30292790c11
Daniel Bagel <db@dbagel.de>
2023-10-11 16:19:16 +0200

    [TASK] Port Code from Form-Relay (MFR-23) (#1)

---
+++ ext_emconf.php
@@ -0,0 +1,21 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Digital Marketing Framwork - Distributor - Pdf',
+    'description' => 'Data Provider for PDFs with fillable form fields',
+    'category' => 'be',
+    'author' => 'Daniel Bagel',
+    'author_email' => 'info@mediatis.de',
+    'author_company' => 'Mediatis AG',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-11.5.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
